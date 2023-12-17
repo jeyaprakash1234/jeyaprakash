155ba91fc8ab17d279197fe28bff23afb8c36ec3
Jeyaparkash <145859559+jeyaprakash1234@users.noreply.github.com>
2023-12-17 12:16:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function foo(){
+    var fname = document.getElementById("firstname").value;
+    console.log(fname);
+    var mname = document.getElementById("middlename").value;
+    console.log(mname);
+    var lname = document.getElementById("lastname").value;
+    console.log(lname);
+    var res = document.getElementById("email").value;
+    console.log(res);
+    }
+
